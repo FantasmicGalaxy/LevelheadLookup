f9c54a3e0fe9b227a59c3bd2518733d57b30dc11
Eric Bishop <futureflyer.eric@gmail.com>
2020-01-17 16:33:06 -0700

    Prettied up the infrastructure, removed styling.

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 const token = '3SAPr11y13BiM7xk';
 const rce = new RumpusCE(token);
 
-const id = 'zchv6z';
+const id = 'bscotch404';
 
 let app = new Vue({
 	el: '#app',
 	data: {
-		userInfo: {}
+		info: {
+			id: {},
+			alias: {},
+			avatarId: {},
+			avatarUrl: {},
+			followers: {},
+			following: {},
+			shipped: {},
+			played: {},
+			shoes: {},
+			ribbons: {},
+			training: {},
+			playsGen: {},
+			playtimeGen: {},
+			builds: {},
+			wins: {},
+			fails: {},
+			trials: {},
+			trophies: {},
+			tipped: {},
+			earned: {}
+		}
 	}
 });
 
-function numberWithCommas(number) {
+function formatCommas(number) {
 	let parts = number.toString().split('.');
 	parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 	return parts.join('.');
 }
 
-function secondsToTime(seconds) {
+function formatTime(seconds) {
 	let minutes = Math.floor(seconds / 60);
 	let hours = Math.floor(minutes / 60);
 	let days = Math.floor(hours / 24);
@@ -30,48 +51,118 @@ function secondsToTime(seconds) {
 	}
 }
 
-async function getUserData(userId) {
-	let userInfo = await rce.levelhead.aliases.search(userId, {}, { doNotUseKey: true });
-	return await userInfo;
+function formatPercent(number) {
+	return `${number}%`;
 }
 
-async function getUserStats(userId) {
-	let userStats = await rce.levelhead.profiles.search({ userIds: userId }, { doNotUseKey: true });
-	return await userStats;
+// fetches and returns the data from Rumpus
+async function getUserData(userId) {
+	try {
+		let aliasData = await rce.levelhead.aliases.search(userId, {}, { doNotUseKey: true });
+		let playerData = await rce.levelhead.players.search({ userIds: userId }, { doNotUseKey: true });
+
+		if (aliasData.length != 1 || playerData.length != 1) {
+			throw new Error('Request from server had too many responses!');
+		}
+
+		let rawData = { ...aliasData[0], ...playerData[0] };
+		return rawData;
+	} catch (err) {
+		console.error(`ERROR: ${err}`);
+	}
 }
 
 async function updateUserInfo(userId) {
-	let userData, userStats;
 	try {
-		userData = await getUserData(userId);
-		userStats = await getUserStats(userId);
+		let rawData = await getUserData(userId);
+
+		let info = {
+			id: {
+				label: 'ID',
+				data: rawData.userId
+			},
+			alias: {
+				label: 'Alias',
+				data: rawData.alias
+			},
+			avatarId: {
+				label: 'Avatar ID',
+				data: rawData.avatarId
+			},
+			avatarUrl: {
+				label: 'Avatar URL',
+				data: `https://img.bscotch.net/fit-in/128x128/avatars/${rawData.avatarId}.png`
+			},
+			followers: {
+				label: 'Followers',
+				data: rawData.stats.Subscribers
+			},
+			following: {
+				label: 'Following',
+				data: rawData.stats.NumFollowing
+			},
+			shipped: {
+				label: 'Shipped',
+				data: rawData.stats.Published
+			},
+			played: {
+				label: 'Levels Played',
+				data: rawData.stats.LevelsPlayed
+			},
+			shoes: {
+				label: 'Shoes',
+				data: rawData.stats.Shoes
+			},
+			ribbons: {
+				label: 'Ribbons',
+				data: rawData.stats.Crowns
+			},
+			training: {
+				label: 'Training Progress',
+				data: rawData.stats.CampaignProg
+			},
+			playsGen: {
+				label: 'Plays Generated',
+				data: rawData.stats.Plays
+			},
+			playtimeGen: {
+				label: 'Playtime Generated',
+				data: rawData.stats.PlayTime
+			},
+			builds: {
+				label: 'Daily Builds Completed',
+				data: rawData.stats.DBComp
+			},
+			wins: {
+				label: 'Wins',
+				data: rawData.stats.Wins
+			},
+			fails: {
+				label: 'Fails',
+				data: rawData.stats.Fails
+			},
+			trials: {
+				label: 'Tower Trials Completed',
+				data: rawData.stats.ChalWins
+			},
+			trophies: {
+				label: 'Time Trophies',
+				data: rawData.stats.TimeTrophies
+			},
+			tipped: {
+				label: 'Exposure Bucks Tipped',
+				data: rawData.stats.BucksTipped
+			},
+			earned: {
+				label: 'Exposure Bucks Earned',
+				data: rawData.stats.TipsGotten
+			}
+		};
+
+		app.info = info;
 	} catch (error) {
 		console.error(`ERROR: ${error}`);
 	}
-
-	let userInfo = {
-		id: userData[0].userId,
-		alias: userData[0].alias,
-		avatar: userData[0].avatarId,
-		followers: numberWithCommas(userStats[0].stats.Subscribers),
-		following: numberWithCommas(userStats[0].stats.NumFollowing),
-		shipped: numberWithCommas(userStats[0].stats.Published),
-		played: numberWithCommas(userStats[0].stats.LevelsPlayed),
-		shoes: numberWithCommas(userStats[0].stats.Shoes),
-		ribbons: numberWithCommas(userStats[0].stats.Crowns),
-		training: numberWithCommas(userStats[0].stats.CampaignProg),
-		playsGen: numberWithCommas(userStats[0].stats.Plays),
-		playtimeGen: secondsToTime(userStats[0].stats.PlayTime),
-		builds: numberWithCommas(userStats[0].stats.DBComp),
-		wins: numberWithCommas(userStats[0].stats.Wins),
-		fails: numberWithCommas(userStats[0].stats.Fails),
-		trials: numberWithCommas(userStats[0].stats.ChalWins),
-		trophies: numberWithCommas(userStats[0].stats.TimeTrophies),
-		tipped: numberWithCommas(userStats[0].stats.BucksTipped),
-		earned: numberWithCommas(userStats[0].stats.TipsGotten)
-	};
-
-	app.userInfo = userInfo;
 }
 
 updateUserInfo(id);
