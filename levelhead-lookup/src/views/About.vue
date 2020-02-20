95e3b1507a1286b2eb88f11fe3e537d74654a4b0
Eric Bishop <futureflyer.eric@gmail.com>
2020-02-19 21:07:00 -0700

    update(structure) Created the app with Vue CLI

---
+++ levelhead-lookup/src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>LevelHead Lookup</h1>
+    <br />
+    <h2>Welcome!</h2>
+    <p>
+      You can use this site to lookup the stats of players and levels, and lots
+      of other stuff too!
+    </p>
+  </div>
+</template>
---
+++ levelhead-lookup/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <main>
+      <h1>LevelHead Lookup</h1>
+      <img
+        alt="Adam's head"
+        src="https://img.bscotch.net/fit-in/128x128/avatars/bscotchadam.png"
+      />
+      <img
+        alt="Seth's head"
+        src="https://img.bscotch.net/fit-in/128x128/avatars/bscotchseth.png"
+      />
+      <img
+        alt="Sam's head"
+        src="https://img.bscotch.net/fit-in/128x128/avatars/bscotchsam.png"
+      />
+    </main>
+  </div>
+</template>
